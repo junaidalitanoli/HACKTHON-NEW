e68b7813c5a75c36409aa9ad203fa4782138cb3a
JUNAIDALITANOLI <junaidalitanoli23@gmail.com>
2025-01-06 12:17:28 +0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs (ES Modules)
 
-export default nextConfig;
+export default {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'your-image-hosting-domain.com', // Replace with the actual image hosting domain
+          port: '',  // Leave this blank unless a specific port is used
+          pathname: '/path-to-images/**',  // Use wildcard (**) to allow all paths under this domain
+        },
+      ],
+    },
+  };
+  
